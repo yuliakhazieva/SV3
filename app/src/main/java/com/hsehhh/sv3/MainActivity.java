558b54ae3126064c450f3336a5744d0a269847de
Nanglo <oppasaranhae@gmail.com>
2018-01-13 11:43:03 +0300

    Initial commit

---
+++ app/src/main/java/com/hsehhh/sv3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hsehhh.sv3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
