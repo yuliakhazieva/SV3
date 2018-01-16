97b709a5f0db4e8e29aa19cd7dfc4a3f64a100cf
Nanglo <oppasaranhae@gmail.com>
2018-01-16 11:52:19 +0300

    creating events like a motherfucker

--- app/src/main/java/com/hsehhh/sv3/ScrollingActivity.java
+++ app/src/main/java/com/hsehhh/sv3/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.hsehhh.sv3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,11 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
+                Intent intent = new Intent(ScrollingActivity.this, CreateEvent.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
