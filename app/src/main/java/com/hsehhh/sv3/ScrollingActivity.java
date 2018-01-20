28d1e8e913ba7053d495e5dfdf58a44727c33ed0
galecore <tima.shunin@gmail.com>
2018-01-20 19:04:01 +0300

    Renamed MainActivity to SettingsActivity. Created new empty MainActivity.

--- app/src/main/java/com/hsehhh/sv3/ScrollingActivity.java
+++ app/src/main/java/com/hsehhh/sv3/ScrollingActivity.java
@@ -14,15 +14,14 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                 //       .setAction("Action", null).show();
+
                 Intent intent = new Intent(ScrollingActivity.this, CreateEvent.class);
                 startActivity(intent);
                 finish();
