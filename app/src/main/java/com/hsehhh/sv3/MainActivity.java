bbd393662a53ed08bd935925d35f6d584526de85
galecore <tima.shunin@gmail.com>
2018-01-21 23:38:00 +0300

    Добавлено ТЗ
    Добавлено техническое задание

--- app/src/main/java/com/hsehhh/sv3/MainActivity.java
+++ app/src/main/java/com/hsehhh/sv3/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.AuthUI.IdpConfig;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +22,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
