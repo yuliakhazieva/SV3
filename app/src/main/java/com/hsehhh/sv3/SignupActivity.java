f0c37cae795a72e8ebcdbef8fb4d54d31ae77dc2
Anna Karbovskaya <karbovskaya.anna@yandex.ru>
2018-02-10 14:03:44 +0300

    minor design and logic changes
    animation added, pictures changed, color sceme changed (note me if you dislike it)

--- app/src/main/java/com/hsehhh/sv3/SignupActivity.java
+++ app/src/main/java/com/hsehhh/sv3/SignupActivity.java
@@ -38,14 +38,6 @@ public class SignupActivity extends AppCompatActivity {
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
-
-        btnResetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
-            }
-        });
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
