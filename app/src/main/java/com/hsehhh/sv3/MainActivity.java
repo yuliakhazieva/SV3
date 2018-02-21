26c19815d0cdf454514f2160d04ad469cf0e2032
galecore <tima.shunin@gmail.com>
2018-02-21 22:08:52 +0300

    Добавил основу для FirebaseUiAuth
    Добавил обработчики событий для регистрации

--- app/src/main/java/com/hsehhh/sv3/MainActivity.java
+++ app/src/main/java/com/hsehhh/sv3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hsehhh.sv3;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -7,13 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.hsehhh.sv3.data.Event;
+import com.hsehhh.sv3.data.User;
 
 public class MainActivity extends AppCompatActivity implements FragmentSwitcher{
+    final static int RC_SIGN_IN = 1;
 
     //UI objects
     Toolbar mainToolbar;
@@ -33,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements FragmentSwitcher{
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     private FirebaseAuth firebaseAuth;
     public FirebaseDatabase firebaseDatabase;
+
+    // User object
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,16 +70,23 @@ public class MainActivity extends AppCompatActivity implements FragmentSwitcher{
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    //  startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                    //  finish();
+                if (user != null) {
+                    onSignedInInitialize(user);
                 } else {
-                    // if user exists
-                    // do something
+//                    startActivityForResult(
+//                            AuthUI.getInstance()
+//                                    .createSignInIntentBuilder()
+//                                    .setIsSmartLockEnabled(false)
+//                                    .build(),
+//                            RC_SIGN_IN);
                 }
-
             }
         };
+    }
+
+    private void onSignedInInitialize(FirebaseUser user) {
+//        user.getProviderData().
+        firebaseDatabase.getReference("users").child(user.getUid());
     }
 
     @Override
@@ -94,6 +109,23 @@ public class MainActivity extends AppCompatActivity implements FragmentSwitcher{
         myEventsFragment = new MyEventsFragment();
         createEventFragment = new CreateEventFragment();
         eventDetailFragment = new EventDetailFragment();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case RC_SIGN_IN:{
+                if (resultCode == RESULT_OK){
+                    Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+                } else if (resultCode == RESULT_CANCELED){
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+            default:{
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
     }
 
     // TODO: Посмотреть, есть ли более гуманный способ переключения на предыдущий фрагмент.
