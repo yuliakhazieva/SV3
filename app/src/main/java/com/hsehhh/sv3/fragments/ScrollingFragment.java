a8dd7d87050ef25c3e827f701fc2dc3b8310b727
Anna Karbovskaya <karbovskaya.anna@yandex.ru>
2018-03-02 08:06:31 +0300

    big description fixed

--- app/src/main/java/com/hsehhh/sv3/fragments/ScrollingFragment.java
+++ app/src/main/java/com/hsehhh/sv3/fragments/ScrollingFragment.java
@@ -130,7 +130,7 @@ public class ScrollingFragment extends android.support.v4.app.Fragment
                                     ib.setImageResource(R.drawable.icon_a_party);
                                     break;
                                 case "request":
-                                    ib.setImageResource(R.drawable.icon_a_help);
+                                    ib.setImageResource(R.drawable.icon_a_study);
                                     break;
                                 default:
                                     ib.setImageResource(R.drawable.icon_a_study);
